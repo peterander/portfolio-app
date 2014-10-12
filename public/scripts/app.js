6003b44786e8181713b0ffd502ab9effc7485020
peterander <paanderson1@gmail.com>
2014-10-12 13:50:08 -0700

    cleanup

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,30 @@
-var app = angular.module('webPortfolio', []);
+(function() {
+  var app = angular.module('webPortfolio', []);
 
-app.controller('PortfolioCtrl', function($scope) {
-  $scope.sites = [
-    {
-      name: 'Travels',
-      description: "Places I've visited around the world.",
-      url: 'http://peterander.github.io/travels/index.html'
-    }, 
-    {
-      name: 'Music',
-      description: "A site about my three instruments.",
-      url: 'http://peterander.github.io/music/index.html'
-    }, 
-    {
-      name: 'Art',
-      description: "Black and white Sharpee drawings.",
-      url: 'http://peterander.github.io/art/index.html'
-    }
-  ];
-});
+  app.controller('PortfolioCtrl', function($scope) {
+    $scope.sites = [{
+        name: 'Travels',
+        description: "Places I've visited around the world.",
+        url: 'http://peterander.github.io/travels/index.html'
+      }, {
+        name: 'Music',
+        description: "A site about my three instruments.",
+        url: 'http://peterander.github.io/music/index.html'
+      }, {
+        name: 'Art',
+        description: "Black and white Sharpee drawings.",
+        url: 'http://peterander.github.io/art/index.html'
+      }];
+  });
 
-app.controller('AddSiteController', function($scope) {
-  $scope.site = {};
 
-  $scope.addSite = function(portfolioItem) {
-    portfolioItem.sites.push($scope.site);
-  };
-});
+  app.controller('SiteController', function($scope) {
+    $scope.site = {};
+
+    $scope.addSite = function() {
+      sites.push($scope.site);
+      $scope.site = {};
+    };
+  });
+
+})();
